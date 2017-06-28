0a56b3796ecab2794a425c08e35e49020e27ad8f
Chao Charlie Huang <chaocharliehuang@gmail.com>
2017-06-27 17:09:08 -0700

    submitted data gets added to table; empty inputs are not allowed

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
-$(document).ready(function() {
-
+$('form').submit(function(e) {
+    var formData = $(this).serializeArray();
+    var tableHTML = "<tr>";
+    formData.forEach(function(userObj) {
+        tableHTML += "<td>" + userObj.value + "</td>";
+    });
+    tableHTML += "</tr>";
+    $('table tbody').append(tableHTML);
+    $('form input').val('');
+    e.preventDefault();
 });
